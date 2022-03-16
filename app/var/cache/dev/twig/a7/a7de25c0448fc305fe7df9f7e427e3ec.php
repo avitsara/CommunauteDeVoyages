38b850849c9a3e2657f75a1cdab1d926e5e91811
avitsara <avitsarajary@yahoo.com>
2022-03-16 22:17:20 +0100

    feat:adding markers to map

--- app/var/cache/dev/twig/a7/a7de25c0448fc305fe7df9f7e427e3ec.php
+++ app/var/cache/dev/twig/a7/a7de25c0448fc305fe7df9f7e427e3ec.php
@@ -72,14 +72,21 @@ class __TwigTemplate_54f1db8a6eb8442447e95559ad0f0023 extends Template
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheet"));
 
         // line 6
-        echo " <link href='https://unpkg.com/maplibre-gl@1.15.2/dist/maplibre-gl.css' rel='stylesheet' />
-<style>
+        echo "<link href=\"https://unpkg.com/maplibre-gl@2.1.7/dist/maplibre-gl.css\" rel=\"stylesheet\" /><style>
 
- .container {
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+\t#map {  width: 100%; }
+  #fly {
+position: relative;
+margin: 0px auto;
+width: 10%;
+height: 0px;
+padding: 0px;
+border: none;
+border-radius: 3px;
+font-size: 12px;
+text-align: center;
+color: #000;
+background: #68707b;
 }
 </style>
 
@@ -89,103 +96,170 @@ class __TwigTemplate_54f1db8a6eb8442447e95559ad0f0023 extends Template
 
     }
 
-    // line 19
+    // line 26
     public function block_javascript($context, array $blocks = [])
     {
         $macros = $this->macros;
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascript"));
 
-        // line 20
-        echo " <script src='https://unpkg.com/maplibre-gl@1.15.2/dist/maplibre-gl.js'></script>
+        // line 27
+        echo "    <script src=\"https://unpkg.com/maplibre-gl@2.1.7/dist/maplibre-gl.js\"></script>
+     <script>
+     /// var styleJson = 'https://api.maptiler.com/maps/voyager/style.json?key=tlsMQxY2O8t8fc48pkeW';
+window.onload = function(){
+   var map = new maplibregl.Map({
+      container: 'map',
+      style: 'https://api.maptiler.com/maps/streets/style.json?key=tlsMQxY2O8t8fc48pkeW',
+      center: [-0.07, 51.5],
+      zoom: 6,
+      hash: true,
+    });
 
+    map.on('load', function () {
+\t\tmap.loadImage(
+\t\t'https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-plane-512.png',
+\t\tfunction (error, image) {
+\t\tif (error) throw error;
+\t\tmap.addImage('AirPort_icon', image);
+        const geojson = {
+'type': 'FeatureCollection',
+'features': [
+{
+'type': 'Feature',
+'geometry': {
+'type': 'Point',
+'coordinates': [-77.032, 38.913]
+},
+'properties': {
+'title': 'Mapbox',
+'description': 'Washington, D.C.'
+}
+},
+{
+'type': 'Feature',
+'geometry': {
+'type': 'Point',
+'coordinates': [-122.414, 37.776]
+},
+'properties': {
+'title': 'Mapbox',
+'description': 'San Francisco, California'
+}
+}
+]
+};
+
+\t\tmap.addSource('AirPorts_points', {
+\t\t\ttype: 'geojson',
+\t\t\tdata: geojson
+\t\t});
+
+\t\tmap.addLayer({
+\t\t'id': 'AirPorts',
+\t\t'type': 'symbol',
+\t\t'source': 'AirPorts_points',
+\t\t'layout': {
+\t\t'icon-image': 'AirPort_icon',
+\t\t'icon-size': 0.05
+\t\t}
+\t\t});
+\t\t}
+\t\t);
+\t\t});
+    
+///var marker = new maplibregl.Marker().setLngLat([0.11, 51.49]).addTo(map);    
+} 
+</script>
+  <script> 
+ 
+</script>
 ";
         
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
 
     }
 
-    // line 25
+    // line 100
     public function block_body($context, array $blocks = [])
     {
         $macros = $this->macros;
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
 
-        // line 26
+        // line 101
         echo twig_include($this->env, $context, "menu.html.twig");
         echo "
 <br>
 ";
-        // line 29
-        echo "<div class=\"card\" style=\"width: 80%;\">
+        // line 104
+        echo "<div class=\"card\" style=\"width: 100%;\">
   <h2> My trips  </h2>
   <div class=\"row flex\">
       ";
-        // line 32
+        // line 107
         $context['_parent'] = $context;
-        $context['_seq'] = twig_ensure_traversable((isset($context["trips"]) || array_key_exists("trips", $context) ? $context["trips"] : (function () { throw new RuntimeError('Variable "trips" does not exist.', 32, $this->source); })()));
+        $context['_seq'] = twig_ensure_traversable((isset($context["trips"]) || array_key_exists("trips", $context) ? $context["trips"] : (function () { throw new RuntimeError('Variable "trips" does not exist.', 107, $this->source); })()));
         foreach ($context['_seq'] as $context["_key"] => $context["trip"]) {
-            // line 33
+            // line 108
             echo "        <div class=\"col-5\">  
             <div class=\"card\">
               <div class=\"card body\">
                  <h5 class=\"card-title\">
                  ";
-            // line 38
+            // line 113
             echo "                 <h4>   ";
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["trip"], "title", [], "any", false, false, false, 38), "html", null, true);
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["trip"], "title", [], "any", false, false, false, 113), "html", null, true);
             echo " </h4>
                  </h5>
               <img class='img-thumbnail rounded float-right' src=\"";
-            // line 40
+            // line 115
             echo twig_escape_filter($this->env, $this->extensions['Vich\UploaderBundle\Twig\Extension\UploaderExtension']->asset($context["trip"], "imageFile"), "html", null, true);
             echo "\" width=\"60%\"/>
                
                   <p class=\"card-text\"> 
                     Départ : ";
-            // line 43
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["trip"], "departure", [], "any", false, false, false, 43), "html", null, true);
+            // line 118
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["trip"], "departure", [], "any", false, false, false, 118), "html", null, true);
             echo "
                   </p>
 
                     <p class=\"card-text\"> 
                     Destination : ";
-            // line 47
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["trip"], "destination", [], "any", false, false, false, 47), "html", null, true);
+            // line 122
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["trip"], "destination", [], "any", false, false, false, 122), "html", null, true);
             echo "
                   </p>
 
                      <p class=\"card-text\"> 
                    Date de départ : ";
-            // line 51
-            echo twig_escape_filter($this->env, twig_date_format_filter($this->env, twig_get_attribute($this->env, $this->source, $context["trip"], "beginDate", [], "any", false, false, false, 51), "Y-m-d"), "html", null, true);
+            // line 126
+            echo twig_escape_filter($this->env, twig_date_format_filter($this->env, twig_get_attribute($this->env, $this->source, $context["trip"], "beginDate", [], "any", false, false, false, 126), "Y-m-d"), "html", null, true);
             echo "
                   </p>
 
                       <p class=\"card-text\"> 
                    Date de retour:  ";
-            // line 55
-            echo twig_escape_filter($this->env, twig_date_format_filter($this->env, twig_get_attribute($this->env, $this->source, $context["trip"], "endDate", [], "any", false, false, false, 55), "Y-m-d"), "html", null, true);
+            // line 130
+            echo twig_escape_filter($this->env, twig_date_format_filter($this->env, twig_get_attribute($this->env, $this->source, $context["trip"], "endDate", [], "any", false, false, false, 130), "Y-m-d"), "html", null, true);
             echo "
                   </p>
                   <p class=\"card-text\"> 
                    Nombre de voyageurs :  ";
-            // line 58
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["trip"], "travelCompanionNumber", [], "any", false, false, false, 58), "html", null, true);
+            // line 133
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["trip"], "travelCompanionNumber", [], "any", false, false, false, 133), "html", null, true);
             echo "
                   </p>
                      <p class=\"card-text\"> 
                    Moyen de transport :  ";
-            // line 61
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["trip"], "transportation", [], "any", false, false, false, 61), "html", null, true);
+            // line 136
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["trip"], "transportation", [], "any", false, false, false, 136), "html", null, true);
             echo "
                   </p>
-                  <p class=\"card-text\">
 
                  
                       ";
-            // line 70
+            // line 144
             echo "              </div>
             </div>
         </div>
@@ -194,24 +268,20 @@ class __TwigTemplate_54f1db8a6eb8442447e95559ad0f0023 extends Template
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['trip'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 74
+        // line 148
         echo "
 
 
   </div>
 </div>
-
 <br>
 
-<div class=\"container\">
-    <div>
-     <iframe  width=\"500\" height=\"500\" src=\"https://api.maptiler.com/maps/voyager/?key=tlsMQxY2O8t8fc48pkeW#0.0/0.00000/0.00000\"></iframe>
+<div id=\"map\"></div>
+  <button id=\"fly\"></button>
 
-    </div>
-</div>
-  
 
-    
+   
+
 ";
         
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
@@ -230,7 +300,7 @@ class __TwigTemplate_54f1db8a6eb8442447e95559ad0f0023 extends Template
 
     public function getDebugInfo()
     {
-        return array (  198 => 74,  189 => 70,  181 => 61,  175 => 58,  169 => 55,  162 => 51,  155 => 47,  148 => 43,  142 => 40,  136 => 38,  130 => 33,  126 => 32,  121 => 29,  116 => 26,  109 => 25,  100 => 20,  93 => 19,  75 => 6,  68 => 5,  55 => 3,  38 => 1,);
+        return array (  272 => 148,  263 => 144,  256 => 136,  250 => 133,  244 => 130,  237 => 126,  230 => 122,  223 => 118,  217 => 115,  211 => 113,  205 => 108,  201 => 107,  196 => 104,  191 => 101,  184 => 100,  107 => 27,  100 => 26,  75 => 6,  68 => 5,  55 => 3,  38 => 1,);
     }
 
     public function getSourceContext()
@@ -240,22 +310,97 @@ class __TwigTemplate_54f1db8a6eb8442447e95559ad0f0023 extends Template
 {% block title %} HomePage {% endblock %}
 
 {% block stylesheet %}
- <link href='https://unpkg.com/maplibre-gl@1.15.2/dist/maplibre-gl.css' rel='stylesheet' />
-<style>
+<link href=\"https://unpkg.com/maplibre-gl@2.1.7/dist/maplibre-gl.css\" rel=\"stylesheet\" /><style>
 
- .container {
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+\t#map {  width: 100%; }
+  #fly {
+position: relative;
+margin: 0px auto;
+width: 10%;
+height: 0px;
+padding: 0px;
+border: none;
+border-radius: 3px;
+font-size: 12px;
+text-align: center;
+color: #000;
+background: #68707b;
 }
 </style>
 
 {% endblock %}
 
 {% block javascript %}
- <script src='https://unpkg.com/maplibre-gl@1.15.2/dist/maplibre-gl.js'></script>
+    <script src=\"https://unpkg.com/maplibre-gl@2.1.7/dist/maplibre-gl.js\"></script>
+     <script>
+     /// var styleJson = 'https://api.maptiler.com/maps/voyager/style.json?key=tlsMQxY2O8t8fc48pkeW';
+window.onload = function(){
+   var map = new maplibregl.Map({
+      container: 'map',
+      style: 'https://api.maptiler.com/maps/streets/style.json?key=tlsMQxY2O8t8fc48pkeW',
+      center: [-0.07, 51.5],
+      zoom: 6,
+      hash: true,
+    });
 
+    map.on('load', function () {
+\t\tmap.loadImage(
+\t\t'https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-plane-512.png',
+\t\tfunction (error, image) {
+\t\tif (error) throw error;
+\t\tmap.addImage('AirPort_icon', image);
+        const geojson = {
+'type': 'FeatureCollection',
+'features': [
+{
+'type': 'Feature',
+'geometry': {
+'type': 'Point',
+'coordinates': [-77.032, 38.913]
+},
+'properties': {
+'title': 'Mapbox',
+'description': 'Washington, D.C.'
+}
+},
+{
+'type': 'Feature',
+'geometry': {
+'type': 'Point',
+'coordinates': [-122.414, 37.776]
+},
+'properties': {
+'title': 'Mapbox',
+'description': 'San Francisco, California'
+}
+}
+]
+};
+
+\t\tmap.addSource('AirPorts_points', {
+\t\t\ttype: 'geojson',
+\t\t\tdata: geojson
+\t\t});
+
+\t\tmap.addLayer({
+\t\t'id': 'AirPorts',
+\t\t'type': 'symbol',
+\t\t'source': 'AirPorts_points',
+\t\t'layout': {
+\t\t'icon-image': 'AirPort_icon',
+\t\t'icon-size': 0.05
+\t\t}
+\t\t});
+\t\t}
+\t\t);
+\t\t});
+    
+///var marker = new maplibregl.Marker().setLngLat([0.11, 51.49]).addTo(map);    
+} 
+</script>
+  <script> 
+ 
+</script>
 {% endblock %}
 
 
@@ -263,7 +408,7 @@ class __TwigTemplate_54f1db8a6eb8442447e95559ad0f0023 extends Template
 {{ include('menu.html.twig')}}
 <br>
 {# LISTE DES VOYAGES CRÉÉS PAR L'UTILISATEUR #}
-<div class=\"card\" style=\"width: 80%;\">
+<div class=\"card\" style=\"width: 100%;\">
   <h2> My trips  </h2>
   <div class=\"row flex\">
       {% for trip in trips %}
@@ -297,7 +442,6 @@ class __TwigTemplate_54f1db8a6eb8442447e95559ad0f0023 extends Template
                      <p class=\"card-text\"> 
                    Moyen de transport :  {{ trip.transportation}}
                   </p>
-                  <p class=\"card-text\">
 
                  
                       {##  
@@ -313,18 +457,15 @@ class __TwigTemplate_54f1db8a6eb8442447e95559ad0f0023 extends Template
 
   </div>
 </div>
-
 <br>
 
-<div class=\"container\">
-    <div>
-     <iframe  width=\"500\" height=\"500\" src=\"https://api.maptiler.com/maps/voyager/?key=tlsMQxY2O8t8fc48pkeW#0.0/0.00000/0.00000\"></iframe>
+<div id=\"map\"></div>
+  <button id=\"fly\"></button>
 
-    </div>
-</div>
-  
 
-    
+   
+
+{###  tlsMQxY2O8t8fc48pkeW #####}
 {% endblock %}
 ", "home/index.html.twig", "/home/fullmetal/Documents/CommunauteDeVoyages/app/templates/home/index.html.twig");
     }
